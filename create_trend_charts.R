1a601de98cd0c24b05964f9ebc0975b8b44f5fdc
vscholl <victoria.scholl@colorado.edu>
2022-10-30 11:46:33 -0600

    created script
    starting with comments

---
+++ create_trend_charts.R
@@ -0,0 +1,8 @@
+# Create trend charts
+
+# This script reads a weather data file (.csv) and outputs a series of
+# trend charts as image files (.png) along with a table (.csv) with
+# the statistical values visualized in each chart.
+
+# User-defined inputs -----------------------------------------------------
+
