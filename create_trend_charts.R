ba444e50f37de2f517736fa0f8605708cdbaa0f6
vscholl <victoria.scholl@colorado.edu>
2022-10-30 12:02:50 -0600

    read csv file
    and specify user-defined date range of interest

--- create_trend_charts.R
+++ create_trend_charts.R
@@ -4,5 +4,26 @@
 # trend charts as image files (.png) along with a table (.csv) with
 # the statistical values visualized in each chart.
 
+
+# install / load required R packages --------------------------------------
+
+library(tidyverse) # readr
+library(dplyr)
+
 # User-defined inputs -----------------------------------------------------
+
+# Specify the file (.csv) containing weather data.
+data_filename <- "data/Kitui_-1.21_38.12_daily.csv"
+
+# Specify the "season" or time period of interest using start/end date "MM-DD"
+# and give the time period a name.
+monthday_start <- "03-15"
+monthday_end <- "10-10"
+season_name <- "15 March - 10 October"
+
+
+# Create charts -----------------------------------------------------------
+
+# Read data file (.csv) into a tibble
+weather_dt <- readr::read_csv(data_filename)
 
