c9512b2c8628413825f8ef10c44e488894479162
vscholl <victoria.scholl@colorado.edu>
2022-11-20 20:18:38 -0700

    starting climatology script
    found old script on Bitbucket as a reference

---
+++ create_climatology_charts.R
@@ -0,0 +1,23 @@
+# Create climatology charts
+
+# This script reads a weather data file (.csv) and outputs a
+# climatology chart visualizing current and long-term normal
+# weekly rainfall.
+
+
+# install / load required R packages --------------------------------------
+
+library(tidyverse) # includes packages such as readr, dplyr, tidyr
+
+
+# User-defined inputs -----------------------------------------------------
+
+
+
+
+# --------------------------------------------------------------------------
+
+
+# CALCULATE LTN WEEKLY RAINFALL
+
+#
